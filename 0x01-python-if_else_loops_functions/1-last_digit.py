339aae7b0bc72d9509506398f75a3f137ca30466
Musa <musaqwabe@gmail.com>
2023-06-06 18:59:21 -0700

    Handled case of negative number and pycodestyle errors

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-expression = "Last digit of {} is {}".format(number, last_digit)
-if number < 0:
-    last_digit *= -1
+l_digit = abs(number) % 10
 
-if last_digit > 5:
-    print("{} and is greater than 5".format(expression))
-elif last_digit == 0:
-    print("{} and is 0".format(expression))
-elif last_digit < 6 and last_digit != 0:
-    print("{} and is less than 6 and not 0".format(expression))
+if number < 0:
+    l_digit *= -1
+
+if l_digit > 5:
+    print(f'Last digit of {number} is {l_digit} and is greater than 5')
+elif l_digit == 0:
+    print(f'Last digit of {number} is {l_digit} and is 0')
+elif l_digit < 6 and l_digit != 0:
+    print(f'Last digit of {number} is {l_digit} and is less than 6 and not 0')
