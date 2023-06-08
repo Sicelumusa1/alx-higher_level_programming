46398036d87d73b0193b8e1c63d3162f74966767
Musa <musaqwabe@gmail.com>
2023-06-08 05:04:15 -0700

    fixed pycodestyle in file 0 and 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import sys
-a = len(sys.argv)
 if __name__ == "__main__":
+    a = len(sys.argv)
     num = 0
     _sum = 0
     for i in range(1, a):
         num = int(sys.argv[i])
         _sum += num
     print(_sum)
-
