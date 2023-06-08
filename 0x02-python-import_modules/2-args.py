6d6491afc40cf9f5c8d07f72e1db4f2eaade55da
Musa <musaqwabe@gmail.com>
2023-06-08 05:53:10 -0700

    Modified file2's logic to consider 0 and 1 args ouside the for loop

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import sys
-a = len(sys.argv)
-for i in range(0, a):
-    if i == 0:
-        print("{} arguments.".format(i))
+if __name__ == "__main__":
+    a = len(sys.argv) - 1
+    if a == 0:
+        print("0 arguments.")
+    elif a == 1:
+        print("1 argument:")
     else:
         print("{} arguments:".format(a))
+    for i in range(1, a + 1):
         print("{}: {}".format(i, sys.argv[i]))
