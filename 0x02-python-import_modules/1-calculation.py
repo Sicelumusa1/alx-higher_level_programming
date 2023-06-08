3298c5cdad54ea82c585a507069dc5b89cbd44b5
Musa <musaqwabe@gmail.com>
2023-06-08 02:00:09 -0700

    Added Readme, task0 and task1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from calculator_1 import add, mul, sub, div
+a = 10
+b = 5
+
+add(a, b)
+sub(a, b)
+mul(a, b)
+div(a, b)
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
