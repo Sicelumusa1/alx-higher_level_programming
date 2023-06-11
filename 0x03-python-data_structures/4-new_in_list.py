1a9345eef086d8f65388ba999e8047455b14bb50
Musa <musaqwabe@gmail.com>
2023-06-11 12:33:24 -0700

    changed the None condition to use 'is' instead of equality

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list == None:
+    if my_list is None or my_list == 0:
         return None
 
     if idx < 0 or idx > len(my_list) - 1:
