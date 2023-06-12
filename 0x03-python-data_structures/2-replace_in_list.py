a358c5aac313157876c26e180ab95b7233c2296f
Musa <musaqwabe@gmail.com>
2023-06-12 01:57:44 -0700

    Modified bound condition to >=

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     if my_list is None or len(my_list) == 0:
         return None
 
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     else:
         my_list[idx] = element
