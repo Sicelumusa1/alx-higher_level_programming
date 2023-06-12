a8ddeb1042f1b324388d07640aa36552e8de3e95
Musa <musaqwabe@gmail.com>
2023-06-12 02:36:35 -0700

    debuged the code to create newlist from empty list and replace with given element where required

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,9 +3,13 @@ def new_in_list(my_list, idx, element):
     if my_list is None or len(my_list) == 0:
         return None
 
-    if idx < 0 or idx > len(my_list) - 1:
+    if idx < 0 or idx >= len(my_list):
         return my_list[:]
     else:
-        new_list = my_list[:]
-        my_list[idx] = element
+        new_list = []
+        for i in range(len(my_list)):
+            if i == idx:
+                new_list.append(element)
+            else:
+                new_list.append(my_list[i])
         return new_list
