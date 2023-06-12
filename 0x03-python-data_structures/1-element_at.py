bdfee75845b46a1c80b9ca0d022fd7626ed708ce
Musa <musaqwabe@gmail.com>
2023-06-12 01:53:32 -0700

    fixed pycodestyle and bound condition

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list is None or len( my_list) == 0:
+    if my_list is None or len(my_list) == 0:
         return None
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
         return my_list[idx]
