04d2621797c6a7156b12a06227ecc93676864e23
Musa <musaqwabe@gmail.com>
2023-06-12 04:28:51 -0700

    added check to see if element being printed is the last one in a row

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix is None or len(matrix) == 0:
+        print()
+        return None
+
+    if len(matrix) == 1 or len(matrix[0]) == 0:
+        print()
         return None
 
     for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end=' ')
+        for j, element in enumerate(i):
+            print("{:d}".format(element), end='')
+            if j != len(i) - 1:
+                print(' ', end='')
         print()
