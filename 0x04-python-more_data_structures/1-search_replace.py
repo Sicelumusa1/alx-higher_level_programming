d9195fa5380614c4afa06ddb62876d1146277397
Musa <musaqwabe@gmail.com>
2023-06-14 13:19:24 -0700

    fixed pycodestyle

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     if my_list is None or len(my_list) == 0:
         return None
-    
+
     new_list = []
     for item in my_list:
         if item == search:
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,6 +2,6 @@
 def common_elements(set_1, set_2):
     if len(set_1) == 0 or len(set_2) == 0:
         return set()
-    
+
     set_3 = set_1.intersection(set_2)
     return set_3
