c6e1b3474a9729cb6a48b72e7a23925bd2460dff
Musa <musaqwabe@gmail.com>
2023-06-14 12:30:14 -0700

    Added file 6 - 11

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    best_key = None
+    best_score = 0
+
+    for key, value in a_dictionary.items():
+        if value is  None:
+            return None
+        if value > best_score:
+            best_key = key
+            best_score = value
+    return best_key
