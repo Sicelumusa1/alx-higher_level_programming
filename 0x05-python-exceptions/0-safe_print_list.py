cf498962a3259a5ffd830998bb5821da9a5aee24
Musa <musaqwabe@gmail.com>
2023-06-26 09:41:18 -0700

    Added readme, task 0 and 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x = 0):
+    
+    if my_list is None:
+        return None
+
+    length = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            length += 1
+    except IndexError:
+        pass
+    print()
+    return length
+
