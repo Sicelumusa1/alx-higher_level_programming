00aa1a64ccdc1427063d6707276f45028eebf424
Musa <musaqwabe@gmail.com>
2023-06-26 14:23:44 -0700

    took care of the use finally

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,13 +15,15 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             result.append(0)
             continue
-
-        if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-             print("wrong type")
-             result.append(0)
-        elif b == 0:
-            print("division by 0")
-            result.append(0)
-        else:
-            result.append(a / b)
+        try:
+            if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+                 print("wrong type")
+                 result.append(0)
+            elif b == 0:
+                print("division by 0")
+                result.append(0)
+            else:
+                result.append(a / b)
+        finally:
+            pass
     return result
