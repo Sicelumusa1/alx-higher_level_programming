a76521a18fe8e1f6f431364ceb8982ae1b32c638
Musa <musaqwabe@gmail.com>
2023-06-27 09:48:53 -0700

    fixed pycodestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,6 +2,8 @@
 """
 Defines a Square class
 """
+
+
 class Square:
     """
     A class representing a square.
@@ -12,10 +14,10 @@ class Square:
     def __init__(self, size=0):
         """
         Initializes a Square object with a given size.
-       
+
        Args:
             size (int): the size of the square (default is 0).
-        
+
         Raises:
             TypeError: If the value is not an integer
             ValueError: If the value is less than 0
@@ -24,4 +26,4 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size    
+        self.__size = size
