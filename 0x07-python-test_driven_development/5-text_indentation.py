7667e898d1cc6f3687b169e884f008efce889af9
Musa <musaqwabe@gmail.com>
2023-07-05 05:28:46 -0700

    fixed pycodestyle

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,11 +13,11 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
+
     i = 0
     while i < len(text) and text[i] == ' ':
         i += 1
-    
+
     while i < len(text):
         print(text[i], end="")
         if text[i] == '\n' or text[i] in ".?:":
