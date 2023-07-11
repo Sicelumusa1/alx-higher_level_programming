8ca3db9ed07004f734eee203b172ce188b967701
Musa <musaqwabe@gmail.com>
2023-07-11 15:17:33 -0700

    fixed a typo in shebang

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,4 @@
-#!/usr/bin/prython3
+#!/usr/bin/python3
 """
 Defines a sript that adds all the arguments to a Python
 list and save it to a JSON file
@@ -9,7 +9,7 @@ import sys
 import json
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('5-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
