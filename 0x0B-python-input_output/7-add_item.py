83360c7917b445a6f769c96cfdb9dd23b5c90867
Musa <musaqwabe@gmail.com>
2023-07-11 15:12:37 -0700

    Altered import statements

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,8 @@ list and save it to a JSON file
 import sys
 import json
 
-save_to_json_file = __import__('save_to_json_file').save_to_json_file
-load_from_json_file = __import__('load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('5-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
