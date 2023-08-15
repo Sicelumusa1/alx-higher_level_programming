9427f6b685e31699567e059c1c69a14fcf6052ce
Musa <musaqwabe@gmail.com>
2023-08-15 10:19:32 -0700

    Added all files

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- List all the tables in hbtn_0c_0
+USE hbtn_0c_0;
+-- List all tables in hbtn_0c_0
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Create the table first_table in the hbtn_0c_0
+USE hbtn_0c_0
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
