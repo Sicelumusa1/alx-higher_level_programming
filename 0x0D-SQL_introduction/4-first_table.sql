4f634791ea1e0f8271ec7dd8225181a5af135c49
Musa <musaqwabe@gmail.com>
2023-08-15 20:54:22 -0700

    modified file 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Create the table first_table in the hbtn_0c_0
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
