3be8bb5cb5e5d068aeedc30ced70124b78b2e208
Sicelumusa <musaqwabe@gmail.com>
2023-09-11 08:40:25 -0700

    Added readme and file 0-4

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const C = 'C is fun';
+const Python = 'Python is cool';
+const JavaScript = 'JavaScript is amazing';
+console.log(C);
+console.log(Python);
+console.log(JavaScript);
