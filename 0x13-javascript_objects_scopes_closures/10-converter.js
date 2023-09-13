9369525d7008418e2f57ecb78c9691b6b0eaad21
Sicelumusa <musaqwabe@gmail.com>
2023-09-12 20:23:51 -0700

    Added file 7-11

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.converter = function (base) {
+  if (base === 10) {
+    return function (number) {
+      return number;
+    };
+  } else {
+    return function (number) {
+      return number.toString(base);
+    };
+  }
+};
