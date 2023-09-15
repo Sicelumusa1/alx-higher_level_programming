35b5587342cb27bbecb26fdeb6204bc11d0686f9
Musa <musaqwabe@gmail.com>
2023-09-15 14:26:15 -0700

    added file 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+This script lists all State objects from the hbtn_0e_6_usa database
+"""
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+
+    #  Extract command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    #  Database connection URI
+    DATABASE_URI = 'mysql://root:root@localhost:3360/hbtn_0e_6_usa'
+    engine = create_engine(DATABASE_URI)
+
+    #  Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #  Query and print all state objects sorted by id
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    #  free resources
+    session.close()
