98696bf299cdaffb18e6ae7e6293522598fc02b9
Musa <musaqwabe@gmail.com>
2023-09-28 04:48:38 -0700

    Added all project files

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Defines a function that finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers using binary search
+
+    Args:
+        list_of_integers (list): A list of unsorted integers
+
+    Returns:
+        int: The peak element found in the list
+    """
+
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            #  The peak in on the left side
+            right = mid
+        else:
+            # the peak is on the right side or at the mid
+            left = mid + 1
+    return list_of_integers[left]
